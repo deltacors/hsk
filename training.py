7848b482e1a4f6c6ebb99e74aaf74902baa431d9
Manuel <info@manuelpellegrinet.com>
2018-12-09 20:17:11 +0100

    The user can save the "words to study in deep" and choose if start a new training or use the saved words

--- training.py
+++ training.py
@@ -7,29 +7,40 @@ words_in_progress = {}
 # words to study in deep
 words_to_study_in_deep = {} 
 
-# count the lessons available
-def lessons_available(wordbase):
-	return len(wordbase)
+# check if there are "words to study in deep" in the beginning of the lesson
+def check_words_to_study_in_deep_file():
+	try:
+		from study_in_deep import words_to_study_in_deep
+		if bool(words_to_study_in_deep):
+			check = input('Do you want to load the saved "words to study in deep" ? [Y/N] ')
+			check = str(check.upper())
+			if check == 'Y':
+				words_in_progress = words_to_study_in_deep
+			elif check == 'N':
+				words_in_progress = select_level(wordbase)
+			else:
+				print('Please type \'Y\' or \'N\'!')
+				check_words_to_study_in_deep_file()
+			return words_in_progress
+	except:
+		return select_level(wordbase)
 
 # select the level of the test
 def select_level(wordbase):
-	check = input('Select your level! Insert a number between 1 and ' + str(lessons_available(wordbase)) + '!')
+	check = input('Until what lesson do you want to test your knowledge? Insert a number between 1 and ' + str(len(wordbase)) + '!')
 	if check != None:
 		try:
    			level = int(check)
-	   		if level < 1 or level > lessons_available(wordbase):
-	   			print('You must insert a number between 1 and ' + str(lessons_available(wordbase)) + '!')
+	   		if level < 1 or level > len(wordbase):
+	   			print('You must insert a number between 1 and ' + str(len(wordbase)) + '!')
 	   			select_level(wordbase)
 		except ValueError:
-		    print('You must insert a number between 1 and ' + str(lessons_available(wordbase)) + '!')
+		    print('You must insert a number between 1 and ' + str(len(wordbase)) + '!')
 		    select_level(wordbase)
-		prepare_wordbase(level)
-
-# prepare the new wordbase
-def prepare_wordbase(level):
-	for l in range(1, level + 1):
-		words_in_progress.update(wordbase['lesson_' + str(l)])
-	return words_in_progress
+		# prepare the level of the new wordbase
+		for l in range(1, level + 1):
+			words_in_progress.update(wordbase['lesson_' + str(l)])
+		return words_in_progress
 
 # loop through the new dictionary of words
 def loop_random_key(words_in_progress):
@@ -37,7 +48,7 @@ def loop_random_key(words_in_progress):
 		word = random.choice(list(words_in_progress))
 		ask_value(word)
 	else:
-		print('Congratulations! You have finished you wordbase. Good job!')
+		print('Congratulations! You have finished your training. Good job!')
 		check_words_to_study_in_deep(words_to_study_in_deep)
 		
 # ask te user the pinyin and the hanzi
@@ -64,7 +75,7 @@ def check_value(word):
 			add_words_to_study_in_deep(word)
 			remove_key(word)
 		else:
-			print('Please type \'Y\' or \'N\' to check your answer!')
+			print('Please type \'Y\' or \'N\'!')
 			check_value(word)
 
 # remove from new dictionary a word already asked
@@ -82,6 +93,9 @@ def check_words_to_study_in_deep(words_to_study_in_deep):
 		for w in words_to_study_in_deep:
 			print(w, ': ', words_to_study_in_deep[w])
 		save_words_to_study_in_deep(words_to_study_in_deep)
+	else:
+		with open('study_in_deep.py', 'w') as file:
+			file.write('')
 
 # save the "words to study in deep" into a file
 def save_words_to_study_in_deep(words_to_study_in_deep):
@@ -89,28 +103,21 @@ def save_words_to_study_in_deep(words_to_study_in_deep):
 	if check != None:
 		check = str(check.upper())
 		if check == 'Y':
-			file = open('study_in_deep.py', 'w')
-			file.write(str(words_to_study_in_deep))
-			file.close()
-			print('File saved!', '\n')
+			with open('study_in_deep.py', 'w') as file:
+				file.write('words_to_study_in_deep = ' + str(words_to_study_in_deep))
+			print('You saved these words!', '\n')
 		elif check == 'N':
-			print('File not saved!', '\n')
+			print('You did not saved these words!', '\n')
 		else:
 			print('Please type \'Y\' or \'N\'!')
 			save_words_to_study_in_deep(words_to_study_in_deep)
 
-# check if there are "words to study in deep" in the beginning of the lesson
-def check_words_to_study_in_deep_file(words_to_study_in_deep):
-	pass
-
 # start here!
 if __name__ == '__main__':
+	print('\n')
 	print('*' * 30, '\n')
 	print('Test your chinese skills!')
 	print('Take pencil and paper and write down the corrispondent pinyin / hanzi value.', '\n')
 	print('*' * 30, '\n')
-	# check_words_to_study_in_deep_file(words_to_study_in_deep)
-	# if yes, words_to_study_in_deep = file
-	# if no , select_level(wordbase)
-	select_level(wordbase)
+	words_in_progress = check_words_to_study_in_deep_file()
 	loop_random_key(words_in_progress)
